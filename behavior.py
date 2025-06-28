8d2c683e119d47a730a1ec73e25064e072b5ebbb
Andres M <andresmejiaro@gmail.com>
2025-06-28 22:06:09 +0200

    Ado

--- behavior.py
+++ behavior.py
@@ -1,6 +1,7 @@
 from ActionTree import GEN, LOGIC
 from action_generators import pick_up, roam
 from totem import mark_totem_gen
+from gathering_plans import gather, drop
 
 # Test Basic actions
 #plan = Basic("soir") & Basic("avance") & Basic("soir") & Basic("droite") & Basic("avance") & Basic("gauche") & Basic("avance") & Basic("soir")
@@ -17,6 +18,14 @@ from totem import mark_totem_gen
 #test eternal roam
 #plan = LOOP(lambda x: roam(x))
 # find food
+lv2invt = ["linemate"]
+lv3invt = ["linemate", "deraumere", "sibur"]
+lv4invt = ["linemate","linemate","sibur","phiras", "phiras"]
+lv5invt = ["linemate", "deraumere","sibur","sibur","phiras"]
+lv6invt = ["linemate", "deraumere","sibur","mendiane","mendiane","mendiane"]
+lv7invt = ["linemate", "deraumere","deraumere","sibur","sibur","sibur","phiras"]
+lv8invt = ["linemate","linemate","deraumere","deraumere","sibur","sibur","mendiane","mendiane","phiras","phiras","thystame"]
+
 find_food_if_hungry = LOGIC(lambda x: x.inventory["norriture"] < 10) & (GEN(lambda x: pick_up(x, "norriture")) |  GEN(lambda x: roam(x)))
 lucky_stone = LOGIC(lambda x: x.inventory["linemate"] < 1) & (GEN(lambda x: pick_up(x, "linemate")) | GEN(lambda x: roam(x)))
 mark_totem = GEN(lambda x: mark_totem_gen(x,"linemate"))
--- gathering_plans.py
+++ gathering_plans.py
@@ -1,9 +1,16 @@
 from Agent import Agent
-from ActionTree import Action
-from action_generators import pick_up_multiple
+from ActionTree import Action, Basic
+from action_generators import pick_up_multiple, gen_basic
+from functools import reduce
 
 def gather(agent: Agent,inventory) -> Action:
     inventory2 = [x for x in inventory if agent.inventory_group[x] == 0]
     if len(inventory) > 0:
         return pick_up_multiple(agent, inventory2)
     
+
+def drop(agent: Agent,inventory) -> Action:
+    actions = map(lambda x: gen_basic("pose",x),inventory)
+    to_return = reduce(lambda x,y: x & y,actions)
+    return to_return
+
